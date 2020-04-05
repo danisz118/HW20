cb15f31ddf47a001fb91f304c2feec616510c66d
danisz118 <daniz118@gmail.com>
2020-04-05 13:31:06 +0300

    add save to file

--- src/main/java/FileStorage.java
+++ src/main/java/FileStorage.java
@@ -1,6 +1,34 @@
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileStorage implements Storage {
+    private String file;
+    private int id;
+    private Map<Integer,String> usersStorage = new HashMap<>();
+    Gson gson = new Gson();
+    public FileStorage(String file){
+        this.file=file;
+        usersTOJson();
+    }
+
+    private void usersTOJson() {
+        saveToFile(gson.toJson(usersStorage));
+    }
+
+    private void saveToFile(String toJson) {
+        try {
+            FileWriter writer = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public void removeAll() {
 
