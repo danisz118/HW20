242f5ed2d788b426eeff3b2cd3e015decd849e06
danisz118 <daniz118@gmail.com>
2020-04-05 12:42:51 +0300

    initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,20 @@
+public class User {
+    protected  int id;
+    protected String name;
+    protected int age;
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
